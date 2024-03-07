23fad90061168bab6e9b2e4480312cc27e0de4a6
gdelvacchio <g.delvacchio1@studenti.unisa.it>
2024-03-07 15:46:28 +0100

    commit di main

--- hello-jsp/src/main/java/Aclass.java
+++ hello-jsp/src/main/java/Aclass.java
@@ -2,6 +2,7 @@
 public class Aclass {
 	/*commento*/
 	public static void main(String[] args) {
-		System.out.println("A message");
+		System.out.println("this is a");
+		System.out.println("mex");
 	}
 }
